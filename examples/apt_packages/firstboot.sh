da63cfe70695ba554d6130b67681c2ada2c995f2
ned <nedmcclain@gmail.com>
2020-10-16 09:21:56 -0600

    Improve `apt update` example per #4

--- examples/apt_packages/firstboot.sh
+++ examples/apt_packages/firstboot.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# update apt cache
-apt-get update
+# update apt cache, ingore "Release file... is not valid yet." error
+apt-get update -o Acquire::Check-Valid-Until=false -o Acquire::Check-Date=false
 
 # install git client and streamer, a camera capture tool
 apt-get install -y git streamer
+
+
+
